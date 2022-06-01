ade5362ed39ec812d8b18a452df6e25f0b98444c
Mudassir742 <mudassirwaheed242@gmail.com>
2022-06-01 15:35:12 +0500

    backend - create contorller for bookmark answer

---
+++ server/src/controllers/bookmarkAnswerController.js
@@ -0,0 +1,48 @@
+const Bookmark = require("../models/bookmarkAnswerModel");
+
+exports.getBookmarkAnswers = async (req, res) => {
+  try {
+    const { userId } = req.user;
+
+    const bookmarkAnswer = await Bookmark.find({ userId: userId }).populate([
+      { path: "userId", select: ["name"] },
+      { path: "answerId", select: ["description", "votes"] },
+    ]);
+    const bookmarkAnswerCount = await Bookmark.countDocuments({}).exec();
+    return res.status(201).json({
+      data: bookmarkAnswer,
+      totalBookmarkAnswers: bookmarkAnswerCount,
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ error: "unexpected server error" });
+  }
+};
+
+exports.getBookmarkAnswerById = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { bookmarkAnswerId } = req.params;
+
+    if (!bookmarkAnswerId || !userId) {
+      return res.status(400).json({ error: "missing input" });
+    }
+
+    const bookmarkAnswerDetail = await Bookmark.findById({
+      _id: bookmarkAnswerId,
+      userId: userId,
+    }).populate([
+      { path: "userId", select: ["name"] },
+      { path: "answerId", select: ["description", "votes"] },
+    ]);
+
+    if (!bookmarkAnswerDetail) {
+      return res.status(404).json({ error: "data not found" });
+    }
+
+    return res.status(201).json({ data: bookmarkAnswerDetail });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ error: "unexpected server error" });
+  }
+};
